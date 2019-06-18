cf713e19d8e12e6cdd45d26c0786578748e545bc
Gowtham <47304952+tummalag@users.noreply.github.com>
2019-06-17 23:39:31 -0400

    inv_kin update

---
+++ inv_kin.py
@@ -0,0 +1,22 @@
+import ctypes
+import os
+import numpy as np
+import time
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+from dynamixel_sdk import *                    # Uses Dynamixel SDK library
