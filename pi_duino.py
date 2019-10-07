57582d0484fe70b1f77fb835e9d57ba5787c9cde
tummalag <47304952+tummalag@users.noreply.github.com>
2019-10-07 15:12:50 -0400

    Update pi_duino.py

--- pi_duino.py
+++ pi_duino.py
@@ -1,21 +1,29 @@
 import serial
 import time
 
-port = "/dev/ttyUSB2"
+port = "/dev/ttyUSB0"
 
 ser = serial.Serial(port,9600)
 ser.flushInput()
 
-comp_list = ["Flash complete\r\n", "Hello\r\n", "Hello inside\r\n"]
 while True:
     if ser.inWaiting()>0:
         inputValue = ser.readline()
         print(inputValue)
-        if inputValue in comp_list:
-            try:
-               n = input("Set the arduino flash times:")
-               ser.write('%d' %n)
-
-            except:
-               print("Input error")
-               ser.write('0')
+        try:
+            rotor = input("Provide Left or Right rotor : ")
+            rotSpeed = int(input("Provide the rotation speed in '%' : "))
+            
+            speedVal = int(rotSpeed*127/100)
+            
+            if rotor == 'Left' | rotor == 'left':
+                sendingByte = 1 << 7 | speedVal
+                
+            else:
+                sendingByte = 0 <<7 | speedVal
+            
+            ser.write('%d' %sendingByte)
+         
+        except:
+             print("Input error")
+                ser.write('0')
