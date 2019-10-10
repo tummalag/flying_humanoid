6478fd457e881a3fd4a6d10946a1200802773da6
tummalag <47304952+tummalag@users.noreply.github.com>
2019-10-10 17:16:13 -0400

    incomplete

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,34 +1,37 @@
+#include <Servo.h>
+
+const int leftRotor = 9;
+const int rightRotor = 8;
+Servo left;
+Servo right;
+const int servoMin = 45;
+const int servoMax = 175;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(leftRotor,OUTPUT);
+  pinMode(rightRotor,OUTPUT);
+  left.attach(leftRotor);
+  right.attach(rightRotor);
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
   Serial.println("Hello");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello inside");
   if(Serial.available()){
-    //flash(Serial.parseInt());
-    int n = Serial.read();
-    Serial.println(n,DEC);
-    Serial.print("Flash times:");
-    Serial.println(n);
-    for(int i = 0; i<n; i++){
-      digitalWrite(13,HIGH);
-      delay(1000);
-      digitalWrite(13,LOW);
-      delay(1000);
-      Serial.println(i+1);
-    }
-    Serial.println("Flash complete");
+    int data = Serial.parseInt();
+    
+    if data && 256 == 0: // means data is for left rotor
+      
+    
     Serial.flush();
   }
   delay(1000);
 }
 
 void flash(int n){
-  Serial.print("Flash times:");
+  Serial.print("");
   Serial.println(n);
   for(int i = 0; i<n; i++){
     digitalWrite(13,HIGH);
